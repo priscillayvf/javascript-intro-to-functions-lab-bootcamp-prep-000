de96a0aadea093fe8559be013ea389d0e78948d0
Priscilla Firpo <priscillayf@gmail.com>
2019-07-22 04:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -130,13 +130,13 @@ function sayHiToGrandma(STRING) {
 //}
 
 function sayHiToGrandma(string) {
-  if (string === string.toUpperCase() {
+  if (string === string.toUpperCase()) {
     return "YES INDEED!"
 }
   else if (string === 'I love you, Grandma.') {
     return "I love you, too."
 } 
-  else if (string === string.toLowerCase() {
+  else if (string === string.toLowerCase()) {
         return "I can't hear you!"
 } 
 }
